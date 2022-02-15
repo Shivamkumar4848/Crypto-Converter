d1fd219f3ac8be6f92a45ad11a7db53f537d493f
Shivamkumar4848 <shivamkumar48482gmail.com>
2022-02-15 20:42:01 +0530

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,14 @@ let crypto = document.getElementById('crypto');
 let btn = document.getElementsByClassName('btn')
 
 const selectCoins = () => {
-    fetch('https://api.coingecko.com/api/v3/coins/list').then(res => res.json())
-        .then(data => {
+    fetch('https://api.coingecko.com/api/v3/coins/list')
+    .then(res => res.json())
+    .then(data => {
         let tempCoins =''
         data.forEach((_, index) => {
             tempCoins += `<option value="${data[index].id}">${data[index].name}</option>`
         })
-        crypto.innerHTML = tempCoins
+    crypto.innerHTML = tempCoins
     })
 
     fetch('https://api.coingecko.com/api/v3/simple/supported_vs_currencies')
@@ -29,7 +30,10 @@ selectCoins()
 const showData = () => {
     fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${crypto.value}&vs_currencies=${currency.value}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data[crypto.value][currency.value])
+        show.innerText = `${currency.value}`+' '+ data[crypto.value][currency.value]
+    })
     .catch(error => console.log(error))
 }
 
